92084cecf89cd4d8a1cb87a14174e1cf8eda2ce7
liamfurphy <liamfurphy@gmail.com>
2014-02-23 16:27:52 +0000

    adding rmc models

--- rate_my_course_project/rate_my_course/models.py
+++ rate_my_course_project/rate_my_course/models.py
@@ -1,3 +1,76 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class University(models.Model):
+    name = models.CharField(max_length=200)
+    email_domain = models.CharField(max_length=50)
+    country = models.CharField(max_length=128, blank=True, null=True)
+    city = models.CharField(max_length=128, blank=True, null=True)
+    telephone = models.CharField(max_length=128, blank=True, null=True)
+
+    def __unicode__(self):
+        return self.name
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User)
+
+    is_email_verified = models.BooleanField(default=False)
+
+
+    class Meta:
+        permissions = (
+            ("can_add_course", "Can add courses"),
+        )
+
+    def __unicode__(self):
+        return self.user.username
+
+
+class Lecturer(models.Model):
+    title = models.CharField(max_length=10)
+    first_name = models.CharField(max_length=128)
+    last_name = models.CharField(max_length=128)
+    email = models.EmailField(null=True, blank=True)
+    department = models.CharField(max_length=128, null=True, blank=True)
+
+    def __unicode__(self):
+        return "{0}. {1} {2}".format(self.title, self.first_name, self.last_name)
+
+
+class Course(models.Model):
+    course_code = models.CharField(max_length=30)
+    course_name = models.CharField(max_length=200)
+    description = models.CharField(max_length=200, null=True, blank=True)
+    year_of_degree = models.IntegerField(null=True, blank=True)
+    number_of_ratings = models.IntegerField()
+
+    average_overall = models.IntegerField()
+    average_difficulty = models.IntegerField()
+    average_teaching = models.IntegerField()
+    average_materials = models.IntegerField()
+    average_satisfaction = models.IntegerField()
+
+    lecturer = models.ForeignKey(Lecturer)
+    uni = models.ForeignKey(University)
+
+    def __unicode__(self):
+        return self.course_name
+
+
+class Rating(models.Model):
+    course = models.ForeignKey(Course)
+    comment = models.CharField(max_length=1000, null=True, blank=True)
+    date = models.DateTimeField()
+
+    overall_rating = models.IntegerField()
+    difficulty_rating = models.IntegerField()
+    teaching_rating = models.IntegerField()
+    materials_rating = models.IntegerField()
+    satisfaction_rating = models.IntegerField()
+
+
+    def __unicode__(self):
+        return "{0}, rating {1}".format(self.course.course_id, self.id)
+
+
