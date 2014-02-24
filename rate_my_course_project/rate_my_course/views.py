cbc9a953c551288ff44e7321b4377871c88f0c0e
Liam Furphy <Liam.Furphy@skyscanner.net>
2014-02-24 12:23:26 +0000

    Passing items to index template

--- rate_my_course_project/rate_my_course/views.py
+++ rate_my_course_project/rate_my_course/views.py
@@ -1,8 +1,14 @@
 from django.template import RequestContext
 from django.shortcuts import render_to_response
+from models import Course, Rating
 
 def index(request):
     # Obtain the context from the HTTP request.
     context = RequestContext(request)
 
-    return render_to_response('index.html',{}, context)
+    top5 = Course.objects.all().filter(average_overall__isnull=False).order_by('-average_overall')[:5]
+    worst5 = Course.objects.all().filter(average_overall__isnull=False).order_by('average_overall')[:5]
+    latest_ratings = Rating.objects.all().order_by("-date")[:5]
+
+
+    return render_to_response('index.html', locals(), context)
