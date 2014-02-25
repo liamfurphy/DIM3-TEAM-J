db06bda5172a39db80d94e9bbb1a45e9759522d3
Liam Furphy <Liam.Furphy@skyscanner.net>
2014-02-25 14:54:10 +0000

    moving to one search bar

--- rate_my_course_project/static/js/base.js
+++ rate_my_course_project/static/js/base.js
@@ -1,22 +1,11 @@
 $(document).ready(function () {
 
     $('#searchform').submit(function validateSearch() {
-        var courseCode = $("#courseCode").val();
-        var courseName = $("#courseName").val();
-        var lecturer = $("#lecturer").val();
-        var university = $("#university").val();
 
-        if (courseCode === "" && courseName === "" && lecturer === "" && university === "") {
-            alert("Please enter a value into at least one search field");
+        if ($('#searchBox').val() === "") {
+            alert("Please enter a value into the search field");
             return false;
         }
-
-        if (courseCode === "") courseCode = "any";
-        if (courseName === "") courseName = "any";
-        if (lecturer === "") lecturer = "any";
-        if (university === "") university = "any";
-
-        $(this).attr("action", "/results/" + courseCode + "/" + courseName + "/" + lecturer + "/" + university + "/");
         return true;
     });
 });
