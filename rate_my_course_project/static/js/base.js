b0fb27bd3fc0df3035d68cb51a8b9096fa0ce4df
Liam Furphy <Liam.Furphy@skyscanner.net>
2014-02-25 14:21:31 +0000

    Updates to the index page

--- rate_my_course_project/static/js/base.js
+++ rate_my_course_project/static/js/base.js
@@ -1,19 +1,22 @@
-function validateSearch(){
-    var courseCode = $("#courseCode").val();
-    var courseName = $("#courseName").val();
-    var lecturer = $("#lecturer").val();
-    var university = $("#university").val();
+$(document).ready(function () {
 
-    if(courseCode === "" && courseName === "" && lecturer === "" && university === ""){
-        alert("Please enter a value into at least one search field");
-        return false;
-    }
+    $('#searchform').submit(function validateSearch() {
+        var courseCode = $("#courseCode").val();
+        var courseName = $("#courseName").val();
+        var lecturer = $("#lecturer").val();
+        var university = $("#university").val();
 
-    if(courseCode === "") courseCode="any";
-    if(courseName === "") courseName="any";
-    if(lecturer === "") lecturer="any";
-    if(university === "") university="any";
+        if (courseCode === "" && courseName === "" && lecturer === "" && university === "") {
+            alert("Please enter a value into at least one search field");
+            return false;
+        }
 
-    $("#searchform").attr("action", "/results/"+courseCode+"/"+courseName+"/"+lecturer+"/"+university+"/");
-    return true;
-}
+        if (courseCode === "") courseCode = "any";
+        if (courseName === "") courseName = "any";
+        if (lecturer === "") lecturer = "any";
+        if (university === "") university = "any";
+
+        $(this).attr("action", "/results/" + courseCode + "/" + courseName + "/" + lecturer + "/" + university + "/");
+        return true;
+    });
+});
