0ff9e93ba19eb54b4e1db3d8f31cdab6454fd21b
Liam Furphy <Liam.Furphy@skyscanner.net>
2014-03-17 15:31:59 +0000

    Updating ticker to user jquery-news JS with custom styling

--- rate_my_course_project/static/js/index.js
+++ rate_my_course_project/static/js/index.js
@@ -1,9 +1,9 @@
 var since;
 $(document).ready(function () {
 
+
     $.get("/api/latest/", function (data) {
-        parse_data(data);
-    });
+        parse_data(data);});
 });
 
 
@@ -13,13 +13,10 @@ function loadSince() {
     });
 }
 function updateTicker(data) {
-    var x = data[0];
-    $("#ticker").prepend("<li class=\"ratingelem col-xs-12 col-md-6 col-lg-4\"><div>" + x.username + " rated " + x.classname +  " " + x.score + "</div></li>");
-    if (data.length > 1) {
-        window.setTimeout(function () {
-            updateTicker(data.slice(1));
-        }, 1000);
-    }
+    $("#ticker-form").append("            <ul id=\"js-news\" class=\"js-hidden\"></ul>");
+    for(var i = 0; i< data.length; i++){
+    var x = data[i];
+    $("#js-news").prepend("<li class=\"ratingelem col-xs-12 col-md-6 col-lg-4\"><div>" + x.username + " rated " + x.classname + " " + x.score + "</div></li>");}
 
 }
 
@@ -27,6 +24,21 @@ function parse_data(data) {
     if (data.length > 0) {
         since = data[0].datestr;
         updateTicker(data);
+        updateticker();
+
     }
-    window.setTimeout(loadSince, 1000);
+        window.setTimeout(loadSince, 15000);
+
+
+}
+
+function updateticker() {
+    $(".ticker-wrapper").remove();
+    $('#js-news').ticker({speed: 0.10,           // The speed of the reveal
+        controls: false,        // Whether or not to show the jQuery News Ticker controls
+        titleText: '',   // To remove the title set this to an empty String
+        displayType: 'fade', // Animation type - current options are 'reveal' or 'fade'
+        pauseOnItems: 3000,    // The pause on a news item before being replaced
+        fadeInSpeed: 600,      // Speed of fade in animation
+        fadeOutSpeed: 300  });
 }
